f0859dd08998ced171a14130756ac07834e0aac1
Glenn Tarcea <glenn.tarcea@gmail.com>
2013-06-01 10:01:42 -0400

    Add module for getting commands to run. Update run script to define environment variable to application etc dir.

---
+++ src/pm_config_file.erl
@@ -0,0 +1,25 @@
+%%% ===================================================================
+%%% @author V. Glenn Tarcea <gtarcea@umich.edu>
+%%%
+%%% @doc API to process config file
+%%%
+%%% @copyright Copyright (c) 2013, Regents of the University of Michigan.
+%%% All rights reserved.
+%%%
+%%% Permission to use, copy, modify, and/or distribute this software for any
+%%% purpose with or without fee is hereby granted, provided that the above
+%%% copyright notice and this permission notice appear in all copies.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+%%% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+%%% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+%%% ===================================================================
+
+-module(pm_config_file).
+-export([]).
+
+%-record()
