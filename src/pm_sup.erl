60a8fe1f998491d151e4c4743a0898715a983e86
Glenn Tarcea <glenn.tarcea@gmail.com>
2013-06-02 14:57:56 -0400

    Add comments in code on how to approach dyanmic creation.

--- src/pm_sup.erl
+++ src/pm_sup.erl
@@ -10,19 +10,34 @@
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(Args), {pm_server, {pm_server, start_link, Args}, permanent, 5000, worker, [pm_server]}).
 
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link({Name, RestartSpec, Jobs}) ->
+    SupervisorName = create_supervisor_name(Name),
+    supervisor:start_link({local, SupervisorName}, ?MODULE, [RestartSpec, Jobs]).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
-init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+init([RestartSpec, Jobs]) ->
+    Children = construct_supervised_children(Jobs),
+    {ok, { {one_for_one, 5, 10}, Children} }.
+
+%% ===================================================================
+%% Local functions
+%% ===================================================================
+create_supervisor_name(JobGroupName) ->
+    list_to_atom(atom_to_list(?MODULE) ++ "_" ++ atom_to_list(JobGroupName)).
+
+%1>lists:seq(1,5).
+%[1,2,3,4,5]
+%2>[ {a,b, X} || X <- lists:seq(1,5)].
+%[{a,b,1},{a,b,2},{a,b,3},{a,b,4},{a,b,5}]
+construct_supervised_children(Jobs) -> [].
+
 
